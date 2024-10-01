7c9dac17669f0c3a022a550b9620b691d23a6dea
UsmanSharif0001 <ussh0001@kea.stud.dk>
2024-10-01 12:04:40 +0200

    Test 2

---
+++ src/main/java/com/example/numtkinoxp/NumtKinoXpApplication.java
@@ -0,0 +1,13 @@
+package com.example.numtkinoxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumtKinoXpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumtKinoXpApplication.class, args);
+    }
+
+}
