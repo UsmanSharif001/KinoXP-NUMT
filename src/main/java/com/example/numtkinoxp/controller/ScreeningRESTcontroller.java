3d79f3613d1beb632f2277eacaf9bb403c5f32e5
NikoP93 <nipe0001@stud.kea.dk>
2024-10-08 10:44:52 +0200

    added some more screening data

--- src/main/java/com/example/numtkinoxp/controller/ScreeningRESTcontroller.java
+++ src/main/java/com/example/numtkinoxp/controller/ScreeningRESTcontroller.java
@@ -3,12 +3,15 @@ package com.example.numtkinoxp.controller;
 import com.example.numtkinoxp.model.Screening;
 import com.example.numtkinoxp.repository.ScreeningRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "*")
 @RestController
 public class ScreeningRESTcontroller {
 
