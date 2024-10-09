b17c41c3d28de03d2e01e386d5e6d9f836f678a2
MuLangballe <mala0007@stud.kea.dk>
2024-10-09 11:54:37 +0200

    Find ticket by id (seatRestController)

--- src/main/java/com/example/numtkinoxp/controller/TicketRESTcontroller.java
+++ src/main/java/com/example/numtkinoxp/controller/TicketRESTcontroller.java
@@ -3,11 +3,16 @@ package com.example.numtkinoxp.controller;
 import com.example.numtkinoxp.model.Ticket;
 import com.example.numtkinoxp.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -20,5 +25,6 @@ public class TicketRESTcontroller {
     public List<Ticket> ticketList() {
         return ticketRepository.findAll();
     }
+
 
 }
