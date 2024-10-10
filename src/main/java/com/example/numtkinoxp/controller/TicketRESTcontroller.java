3f2e0e42847943c1f5ea07d14d7ea1ee0baf5083
MuLangballe <mala0007@stud.kea.dk>
2024-10-10 15:06:27 +0200

    Find ticket by id (seatRestController)

--- src/main/java/com/example/numtkinoxp/controller/TicketRESTcontroller.java
+++ src/main/java/com/example/numtkinoxp/controller/TicketRESTcontroller.java
@@ -5,10 +5,7 @@ import com.example.numtkinoxp.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +21,16 @@ public class TicketRESTcontroller {
     @GetMapping("/ticket")
     public List<Ticket> ticketList() {
         return ticketRepository.findAll();
+    }
+    @PostMapping("/postticket")
+    public ResponseEntity<Ticket> postTicket(@RequestBody Ticket ticket) {
+        System.out.println("Indsætter ny Ticket" + ticket);
+        Ticket savedTicket = ticketRepository.save(ticket);
+        if (savedTicket == null) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        } else {
+            return new ResponseEntity<>(savedTicket, HttpStatus.CREATED);
+        }
     }
 
 
